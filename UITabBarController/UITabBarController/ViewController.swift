4e7a876e608f05166f66b0d9369f81baa5becb2e
Nikolai Ostonal <lnostonal@gmail.com>
2019-07-18 11:20:57 +0900

    fifty questions kadai
    save point currently on question 20

---
+++ UITabBarController/UITabBarController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITabBarController
+//
+//  Created by nikolai ostonal on 2019/07/18.
+//  Copyright © 2019 nikolai ostonal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
