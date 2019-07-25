9dbb12c0169d16dcbf5602fbcf74121537c06dce
nikolaiotsu <35322326+nikolaiotsu@users.noreply.github.com>
2019-07-25 10:16:03 +0900

    Add files via upload

---
+++ UINavigationController.23/UINavigationController.23/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UINavigationController.23
+//
+//  Created by nikolai ostonal on 2019/07/24.
+//  Copyright © 2019 nikolai ostonal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
